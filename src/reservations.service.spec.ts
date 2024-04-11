d07ec3b7be64fefab5e1bdcc65dd404fc34624a0
Kushal Gaur <501853@containerstore.com>
2024-04-11 15:05:28 +0530

    updated reservation service

--- src/reservations.service.spec.ts
+++ src/reservations.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+//import { User } from './entities/user.entity';
 import { ReservationsService } from './reservations.service';
 
-describe('ReservationsService', () => {
+describe('UsersService', () => {
   let service: ReservationsService;
+
+  const mockUserRepository = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReservationsService],
+      providers: [
+        ReservationsService,
+        {
+          provide: getRepositoryToken(() => {}),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ReservationsService>(ReservationsService);
@@ -15,4 +30,11 @@ describe('ReservationsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('create', () => {});
+
+  it('findAll', () => {});
+  it('findOne', () => {});
+  it('update', () => {});
+  it('remove', () => {});
 });
